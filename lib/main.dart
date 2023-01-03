9bf98915ca407f23ac6076db821f694689e5fb20
Kushimo Bashir <72566175+sublime247@users.noreply.github.com>
2023-01-03 10:40:16 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+ import 'package:flutter/material.dart';
+ void main(List<String> args) {
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
